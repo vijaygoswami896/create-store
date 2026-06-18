45dc27f07e2f4eec97afde388f0dbe07133bc06b
hppavelion <hppavelion1819@gmail.com>
2026-06-18 12:24:48 +0530

    testing correct github account

---
+++ src/main/java/com/example/demo/entities/Product.java
@@ -0,0 +1,43 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Product name is required")
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    private String category;
+
+    @NotNull(message = "Price is required")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Price must be greater than zero")
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @NotNull(message = "Stock quantity is required")
+    @Min(value = 0, message = "Stock can not be less than 0")
+    @Column(name = "stock_quantity", nullable = false)
+    private Integer stockQuantity;
+
+    //TODO relations
+}
