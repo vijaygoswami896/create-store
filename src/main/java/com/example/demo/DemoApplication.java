9bd8b1f5c7fea053bc4a00ad9d3cfe40f6d7a2b5
hppavelion <hppavelion1819@gmail.com>
2026-06-18 11:28:54 +0530

    chore: add dotenv java for the application

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		//Configuration of dotenv-java
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+   		dotenv.entries().forEach(dotenvEntry -> System.setProperty(dotenvEntry.getKey(),dotenvEntry.getValue()));
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
